49d31b798263e3a59127e5886c9f4606228b37e3
TheGenial <arifulislam14.ict@gmail.com>
2020-03-14 19:05:46 +0600

    friendsbook complete project added.
    friendsbook complete project added.

---
+++ friend-collection/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Components/Header/Header'
+import FriendList from './Components/FriendList/FriendList'
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <FriendList></FriendList>
+    </div>
+  );
+}
+
+export default App;
---
+++ friend-collection/src/fakeData/Data.js
@@ -0,0 +1,140 @@
+var friendsData =
+    [
+        {
+            "id": 1,
+            "name": "Jeff Bezos",
+            "designation": "Founder and CEO, Amazon",
+            "email": "bezos@amazon.co",
+            "phone": "1-770-736-8031 x56442",
+            "income": 5000,
+            "image": "https://i.ibb.co/D7pFfBB/1.jpg"
+        },
+        {
+            "id": 2,
+            "name": "Tim Cook",
+            "designation": "CEO, Apple",
+            "email": "cook@apple.co",
+            "phone": "010-692-6593 x09125",
+            "income": 4500,
+            "image": "https://i.ibb.co/wQS53sd/2.png"
+        },
+        {
+            "id": 3,
+            "name": "Satya Nadella",
+            "designation": "CEO, Microsoft",
+            "email": "nadella@microsoft.net",
+            "phone": "1-463-123-4447",
+            "income": 3000.75,
+            "image": "https://i.ibb.co/BK4vHY4/3.jpg"
+        },
+        {
+            "id": 4,
+            "name": "Jack Ma",
+            "designation": "Founder and executive chairman, Alibaba Group",
+            "email": "jackma@alibaba.org",
+            "phone": "493-170-9623 x156",
+            "income": 4570.45,
+            "image": "https://i.ibb.co/rp0L0bL/4.png"
+        },
+        {
+            "id": 5,
+            "name": "Palmer Luckey",
+            "designation": "Founder, Oculus",
+            "email": "lucky@annie.ca",
+            "phone": "(254)954-1289",
+            "income": 5200.98,
+            "image": "https://i.ibb.co/0n986jt/5.jpg"
+        },
+        {
+            "id": 6,
+            "name": "Marc Benioff",
+            "designation": "Founder and co-CEO, Salesforce",
+            "email": "marc@salesforce.info",
+            "phone": "1-477-935-8478 x6430",
+            "income": 1230.67,
+            "image": "https://i.ibb.co/GpYbgf3/6.jpg"
+        },
+        {
+            "id": 7,
+            "name": "Ren Zhengfei",
+            "designation": "Founder and CEO, Huawei",
+            "email": "ran@huawei.mo",
+            "phone": "210.067.6132",
+            "income": 1500,
+            "image": "https://i.ibb.co/42L6zFG/7.jpg"
+        },
+        {
+            "id": 8,
+            "name": "Peter Sun",
+            "designation": "Chairman and CEO, Inspur",
+            "email": "sun@inspur.co",
+            "phone": "586.493.6943 x140",
+            "income": 6500.75,
+            "image": "https://i.ibb.co/cbnkhN9/8.jpg"
+        },
+        {
+            "id": 9,
+            "name": "Ginni Rometty",
+            "designation": "CEO, IBM",
+            "email": "ginni@ibm.org",
+            "phone": "(775)976-6794 x41206",
+            "income": 3500.25,
+            "image": "https://i.ibb.co/2YXbjLz/9.jpg"
+        },
+        {
+            "id": 10,
+            "name": "Robert Swan",
+            "designation": "CEO, Intel",
+            "email": "swang@intel.org",
+            "phone": "024-648-3804",
+            "income": 4500,
+            "image": "https://i.ibb.co/1b6Xk1T/10.jpg"
+        },
+        {
+            "id": 11,
+            "name": "Duncan Epping",
+            "designation": "CTO, VMware",
+            "email": "duncan.epping@vmware.biz",
+            "phone": "024-648-3804",
+            "income": 6500,
+            "image": "https://i.ibb.co/zhn1yYs/11.jpg"
+        },
+        {
+            "id": 12,
+            "name": "Stewart Butterfield",
+            "designation": "Founder, Slack",
+            "email": "stewart@slack.org",
+            "phone": "024-648-3804",
+            "income": 2700,
+            "image": "https://i.ibb.co/10ZSNhV/12.jpg"
+        },
+        {
+            "id": 13,
+            "name": "Jensen Huang",
+            "designation": "Founder and CEO, Nvidia",
+            "email": "huang@nvidia.org",
+            "phone": "024-648-3804",
+            "income": 3200.02,
+            "image": "https://i.ibb.co/MP6gLhR/13.jpg"
+        },
+        {
+            "id": 14,
+            "name": "Meredith Whittaker",
+            "designation": "AI researcher, formerly of Google",
+            "email": "meredith@google.org",
+            "phone": "024-648-3804",
+            "income": 4200,
+            "image": "https://i.ibb.co/Zgjhh1V/14.jpg"
+        },
+        {
+            "id": 15,
+            "name": "Andy Jassy",
+            "designation": "CEO, AWS",
+            "email": "jassy@aws.org",
+            "phone": "024-648-3804",
+            "income": 5300,
+            "image": "https://i.ibb.co/BtfdCYZ/15.jpg"
+        }
+    ];
+
+export default friendsData;
